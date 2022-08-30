d55b8f9b6b1f24771b4c14b3a065c69f653cc1db
Vitória Moura <vim.saturnino@gmail.com>
2022-08-30 19:13:13 -0300

    validations added to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,16 @@
 class Student < ApplicationRecord
   has_many :Registrations
+
+  validates :name, presence: true, uniqueness: true
+  validates :cpf, numericality: true, uniqueness: true 
+  validates :genre, if: :valid_gender_type?
+  validates :form_of_payment, if: :valid_payment_type?
+
+  def valid_gender_type?
+    genre in ['F', 'M', 'O']
+  end 
+
+  def valid_payment_type?
+    form_of_payment in ['ticket', 'card']
+  end
 end
