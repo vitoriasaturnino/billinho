528184f010b3f85e7d7691993b1110ef3f1bc770
Vitória Moura <vim.saturnino@gmail.com>
2022-08-30 19:50:03 -0300

    validations added to registration  model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,12 @@
 class Registration < ApplicationRecord
   belongs_to :EducationalInstitution
   belongs_to :Student
-end
 
-# pertence a uma apenas uma instituição
+  validates :amount, presence: true, comparison: { greater_than: 0 }
+  validates :invoice_quantity, presence: true, comparison: { greater_than_or_equal_to: 1 }
+  validates :expiration_day, presence: true, comparison: { greater_than_or_equal_to: 1 && less_than_or_equal_to: 31 }
+  validates :course_name, presence: true
+
+  validates :EducationalInstitutional
+  validates :Student
+end
