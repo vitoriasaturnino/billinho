8ac777dcbf66857d4cb74ef59fd7e224b510311b
Vitória Moura <vim.saturnino@gmail.com>
2022-08-30 19:50:44 -0300

    validations added to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,13 @@
 class Invoice < ApplicationRecord
   belongs_to :Registrations
+
+  validates invoice_amount, presence: true
+  validates due_date, presence: true
+  validates status, presence: true, if: :valid_status?, default: 'open'
+
+  validates :Registrations
+
+  def valid_status?
+    status in ['open', 'late', 'paid']
+  end
 end
