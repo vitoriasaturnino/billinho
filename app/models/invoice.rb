0c2f57645d0db27e2ae9e075ceb36a311bdeab9c
Vitória Moura <vim.saturnino@gmail.com>
2022-08-31 21:42:48 -0300

    refactoring validations to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,10 +4,4 @@ class Invoice < ApplicationRecord
   validates invoice_amount, presence: true
   validates due_date, presence: true
   validates status, presence: true, if: :valid_status?, default: 'open'
-
-  validates :Registrations
-
-  def valid_status?
-    status in ['open', 'late', 'paid']
-  end
 end
