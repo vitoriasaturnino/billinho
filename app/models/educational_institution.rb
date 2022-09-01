d0c72297aaff2dd5c308c93c76beabaedc5092a2
Vitória Moura <vim.saturnino@gmail.com>
2022-08-31 21:38:16 -0300

    refactoring validations to educationalInstitution model

--- app/models/educational_institution.rb
+++ app/models/educational_institution.rb
@@ -2,10 +2,6 @@ class EducationalInstitution < ApplicationRecord
   has_many :Registrations
 
   validates :name, presence: true, uniqueness: true
-  validates :cnpj, numericality: true, uniqueness: true 
-  validates :type, if: :university_school_nursery?
-
-  def university_school_nursery?
-    type in ['university', 'school', 'nursery']
-  end
+  validates :cnpj, numericality: { only_integer: true }, length: { is: 11 }, uniqueness: true 
+  validates :type, inclusion: { in: %w[Nursery School University] }
 end
