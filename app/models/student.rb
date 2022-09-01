b464029dab545d8c7e5a78fbb193693bbb35f170
Vitória Moura <vim.saturnino@gmail.com>
2022-09-01 16:45:51 -0300

    refactoring validations to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,15 +2,7 @@ class Student < ApplicationRecord
   has_many :Registrations
 
   validates :name, presence: true, uniqueness: true
-  validates :cpf, numericality: true, uniqueness: true 
-  validates :genre, if: :valid_gender_type?
-  validates :form_of_payment, if: :valid_payment_type?
-
-  def valid_gender_type?
-    genre in ['F', 'M', 'O']
-  end 
-
-  def valid_payment_type?
-    form_of_payment in ['ticket', 'card']
-  end
+  validates :cpf, numericality: { only_integer: true }, length: { is: 11 }, uniqueness: true 
+  validates :genre, inclusion: { in: %w[F M O] }
+  validates :form_of_payment, inclusion: { in: %w[ticket card] }
 end
