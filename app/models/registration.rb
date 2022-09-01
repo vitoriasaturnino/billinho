1678fc8b68520171855f1c7cdbb02b021b950bea
Vitória Moura <vim.saturnino@gmail.com>
2022-09-01 16:37:55 -0300

    refactoring validations to registration model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,12 +1,10 @@
 class Registration < ApplicationRecord
   belongs_to :EducationalInstitution
   belongs_to :Student
+  has_many :invoice_quantity
 
-  validates :amount, presence: true, comparison: { greater_than: 0 }
-  validates :invoice_quantity, presence: true, comparison: { greater_than_or_equal_to: 1 }
-  validates :expiration_day, presence: true, comparison: { greater_than_or_equal_to: 1 && less_than_or_equal_to: 31 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :invoice_quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :expiration_day, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
   validates :course_name, presence: true
-
-  validates :EducationalInstitutional
-  validates :Student
 end
