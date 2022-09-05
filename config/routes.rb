c1a7c4c04c1a691ddfc048658a726d2e7fd22efa
Vitória Moura <vim.saturnino@gmail.com>
2022-09-05 17:33:10 -0300

    add routes to students

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   post "educational_institution", to: "educational_institution#create"
   get "educational_institution", to: "educational_institution#index"
   get "educational_institution/:id", to: "educational_institution#show"
+
+  # Students Routes
+  post "student", to: "students#create"
+  get "student", to: "students#index"
+  get "student/:id", to: "students#show"
 end
+
