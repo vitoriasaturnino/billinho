6210331ea00fbb863ba3fc55547acf373675393f
Vitória Moura <vim.saturnino@gmail.com>
2022-09-09 18:18:18 -0300

    refactorin registration model to create invoices

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,6 @@
 class Registration < ApplicationRecord
+  require "date"
+
   belongs_to :educational_institution
   belongs_to :student
   has_many :invoice
@@ -7,4 +9,29 @@ class Registration < ApplicationRecord
   validates :invoice_quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :expiration_day, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
   validates :course_name, presence: true
+
+  after_create :create_invoices
+
+  def create_invoices
+    invoice_amount = amount / invoice_quantity
+    date = Time.now.to_date
+    current_day = date.day
+
+    if expiration_day >= current_day
+      due_date = date.strftime("#{due_date}/%m/%y").to_date
+    else
+      due_date = date.next_month.strftime("#{due_date}/%m/%y").to_date
+    end
+
+    Invoice.create(invoice_amount: invoice_amount, due_date: due_date, status: "open", registration_id: id)
+
+    invoice_index = 1
+
+    while invoice_index < invoice_quantity
+      due_date = Date.new(due_date.year, due_date.month, due_date.day).next_month(invoice_index)
+      invoice_index += 1
+      Invoice.create(invoice_amount: invoice_amount, due_date: due_date, status: "open", registration_id: id)
+    end
+  end
 end
+
