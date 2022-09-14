de8974e6c76fe051a67d76fa9cfb844a33710a21
Vitória Moura <vim.saturnino@gmail.com>
2022-09-14 17:48:41 -0300

    fix function index

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -1,9 +1,7 @@
 class InvoiceController < ApplicationController
   def index
-    def show
-      invoice = Invoice.find(params[:id])
-      render json: {status: 'SUCCESS', message:'Loaded invoices', data:invoice},status: :ok
-    end
+    invoice = Invoice.order('created_at DESC');
+    render json: {status: 'SUCCESS', message:'invoices listed', data:invoice}, status: :ok
   end
 
   def show
