05187b896b67cd40319e648d380031432f46bb34
Vitória Moura <vim.saturnino@gmail.com>
2022-09-26 14:07:32 -0300

    refactor: logic of invoices cration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,20 +5,20 @@ class RegistrationsController < ApplicationController
     registration = Registration.new(registration_params)
 
     if registration.save && create_invoices(registration_params, registration.id)
-      render json: {status: 'SUCCESS', message:'Saved registration', data:registration}, status: :ok
+      render json: {status: 'SUCCESS', message:'Saved registration', date:registration}, status: :ok
     else
-      render json: {status: 'ERROR', message:'registration not saved', data:registration.errors}, status: :unprocessable_entity
+      render json: {status: 'ERROR', message:'registration not saved', date:registration.errors}, status: :unprocessable_entity
     end
   end
 
   def index
     registration = Registration.order('created_at DESC');
-    render json: {status: 'SUCCESS', message:'registrations listed', data:registration}, status: :ok
+    render json: {status: 'SUCCESS', message:'registrations listed', date:registration}, status: :ok
   end
 
   def show
     registration = Registration.find(params[:id])
-    render json: {status: 'SUCCESS', message:'Loaded registration', data:registration}, status: :ok
+    render json: {status: 'SUCCESS', message:'Loaded registration', date:registration}, status: :ok
   end
 
   # Parametros aceitos
@@ -43,13 +43,13 @@ class RegistrationsController < ApplicationController
   end
 
   def validate_due_date(expiration_day)
-    date = Time.now.to_date
+    date = DateTime.now.to_date
     current_day = date.day
     
     if expiration_day >= current_day
       due_date = date.strftime("#{expiration_day}/%m/%Y").to_date
     else
-      due_date = date.data.next_month.strftime("#{expiration_day}/%m/%Y").to_date
+      due_date = date.date.next_month.strftime("#{expiration_day}/%m/%Y").to_date
     end 
   end
 
